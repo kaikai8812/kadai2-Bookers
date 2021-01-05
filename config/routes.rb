83f2a479a4ee824b53cffda3442fe767f0f3372a
kaikai8812 <sauna.kaikai@gmail.com>
2021-01-05 05:34:37 +0000

    エラーメッセージの追加のみ

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
   root :to => 'books#top'
   get 'books/index' => 'books#index', as: 'books'
   get 'books/:id' => 'books#show', as: 'book'
-  get 'books/edit/:id' => 'books#edit', as: 'book_edit'
+  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
   post 'books/index' => 'books#create'
   patch 'books/:id' => 'books#update'
   delete 'books/:id' => 'books#destroy', as: 'book_destroy'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
