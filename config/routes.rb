1f046761bb75136b18d6487d45808c0c744b315a
kaikai8812 <sauna.kaikai@gmail.com>
2021-01-06 05:04:07 +0000

    20210106 kadai2-1

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
   root :to => 'books#top'
-  get 'books/index' => 'books#index', as: 'books'
-  get 'books/:id' => 'books#show', as: 'book'
-  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
-  post 'books/index' => 'books#create'
-  patch 'books/:id' => 'books#update'
-  delete 'books/:id' => 'books#destroy', as: 'book_destroy'
+  # get 'books' => 'books#index', as: 'books'
+  # get 'books/:id' => 'books#show', as: 'book'
+  # get 'books/:id/edit' => 'books#edit', as: 'edit_book'
+  # post 'books/index' => 'books#create'
+  # # 上記のbooks/index部分がよろしくない？これがあるとエラーメッセージのとき、URLが変化してしまう？
+  # patch 'books/:id' => 'books#update'
+  # delete 'books/:id' => 'books#destroy', as: 'book_destroy'
+  
+  resources :books
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
